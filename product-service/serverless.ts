2e2667c580329d7e5891c1ccf6a6e297297d7c04
Vishnu Deekshit Holachi <vishnudeekshit@gmail.com>
2023-03-03 02:42:55 +0530

    feat: corrected task3

---
+++ product-service/serverless.ts
@@ -0,0 +1,41 @@
+import type { AWS } from '@serverless/typescript';
+
+import getProductsListTS  from '@functions/getProductsList';
+import getProductByIdTS  from '@functions/getProductByID';
+
+const serverlessConfiguration: AWS = {
+  service: 'product-service',
+  frameworkVersion: '3',
+  plugins: ['serverless-esbuild', 'serverless-auto-swagger', 'serverless-offline'],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs14.x',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+      TABLE_PRODUCTS:'products',
+      TABLE_STOCKS:'stocks'
+    },
+  },
+  // import the function via paths
+  functions: { getProductsListTS, getProductByIdTS },
+  package: { individually: true },
+  custom: {
+    esbuild: {
+      bundle: true,
+      minify: false,
+      sourcemap: true,
+      exclude: ['aws-sdk'],
+      target: 'node14',
+      define: { 'require.resolve': undefined },
+      platform: 'node',
+      concurrency: 10,
+    },
+  },
+};
+
+module.exports = serverlessConfiguration;
