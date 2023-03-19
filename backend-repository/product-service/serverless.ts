c37db803b0e87e38ab85da6a1eb64fc514fcc9e2
Vishnu Deekshit Holachi <vishnudeekshit@gmail.com>
2023-03-20 00:40:42 +0530

    feat: SNS AND SQS implementation

--- backend-repository/product-service/serverless.ts
+++ backend-repository/product-service/serverless.ts
@@ -1,8 +1,9 @@
 import type { AWS } from '@serverless/typescript';
 
-import getProductsListTS  from '@functions/getProductsList';
-import getProductByIdTS  from '@functions/getProductByID';
-import createProductTS  from '@functions/createProduct';
+import getProductsListTS from '@functions/getProductsList';
+import getProductByIdTS from '@functions/getProductByID';
+import createProductTS from '@functions/createProduct';
+import catalogBatchProcess from '@functions/catalogBatchProcess';
 
 const serverlessConfiguration: AWS = {
   service: 'product-service',
@@ -10,20 +11,78 @@ const serverlessConfiguration: AWS = {
   plugins: ['serverless-esbuild', 'serverless-auto-swagger', 'serverless-offline'],
   provider: {
     name: 'aws',
+    region: 'us-east-1',
+    stage: 'dev',
     runtime: 'nodejs14.x',
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
     },
+    iam: {
+      role: {
+        statements: [
+          {
+            Effect: "Allow",
+            Action: ["sqs:*"],
+            Resource: {
+              "Fn::GetAtt": ["catalogItemsQueue", "Arn"]
+            }
+          },
+          {
+            Effect: "Allow",
+            Action: ["sns:*"],
+            Resource: {
+              Ref: 'createProductTopic'
+            }
+          }
+        ]
+      }
+    },
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
-      TABLE_PRODUCTS:'products',
-      TABLE_STOCKS:'stocks'
+      TABLE_PRODUCTS: 'products',
+      TABLE_STOCKS: 'stocks',
+      TOPIC_NAME: 'createProductTopic',
+      TOPIC_ARN: 'arn:aws:sns:${self:provider.region}:314276015768:${self:provider.environment.TOPIC_NAME}'
     },
   },
+  resources: {
+    Resources: {
+      catalogItemsQueue: {
+        Type: 'AWS::SQS::Queue',
+        Properties: {
+          QueueName: 'catalogItemsQueue',
+        }
+      },
+      createProductTopic: {
+        Type: 'AWS::SNS::Topic',
+        Properties: {
+          TopicName: 'createProductTopic'
+        }
+      },
+      createProductSNSSubscription: {
+        Type: 'AWS::SNS::Subscription',
+        Properties: {
+          TopicArn: { Ref: 'createProductTopic' },
+          Endpoint: 'vishnudeekshit@gmail.com',
+          Protocol: 'email'
+        }
+      },
+      createFilteredProductSNSSubscription: {
+        Type: 'AWS::SNS::Subscription',
+        Properties: {
+          TopicArn: { Ref: 'createProductTopic' },
+          Endpoint: 'vishnu.krakow@gmail.com',
+          Protocol: 'email',
+          FilterPolicyScope: 'MessageBody',
+          FilterPolicy: '{"price":[{"numeric":[">=",30]}]}'
+        }
+      },
+    }
+  },
   // import the function via paths
-  functions: { getProductsListTS, getProductByIdTS, createProductTS },
+  functions: { getProductsListTS, getProductByIdTS, createProductTS, catalogBatchProcess },
   package: { individually: true },
   custom: {
     esbuild: {
---
+++ backend-repository/product-service/src/functions/catalogBatchProcess/handler.ts
@@ -0,0 +1,74 @@
+import { SQSEvent, SQSHandler } from 'aws-lambda';
+import {
+    DynamoDBClient,
+    TransactWriteItemsCommandInput, TransactWriteItemsCommand
+} from '@aws-sdk/client-dynamodb';
+import { SNSClient, PublishCommand, PublishCommandInput} from '@aws-sdk/client-sns';
+import { marshall, unmarshall } from '@aws-sdk/util-dynamodb';
+import { v4 as uuidv4 } from 'uuid';
+
+const catalogBatchProcess: SQSHandler = async (event: SQSEvent) => {
+
+    try {
+        for (const record of event.Records) {
+            const body = JSON.parse(record.body);
+            const productId = uuidv4();
+
+            const dbClient = new DynamoDBClient({ region: 'us-east-1' });
+            
+            const pItem = marshall({ id: productId, title: body.title || {}, description: body.description || {}, price: parseInt(body.price) || {} });
+            const sItem = marshall({ product_id: productId, count: parseInt(body.count) || {} });
+
+            const trCommand: TransactWriteItemsCommandInput = {
+                TransactItems: [
+                    {
+                        Put: {
+                            Item: pItem,
+                            TableName: process.env.TABLE_PRODUCTS
+                        }
+                    },
+                    {
+                        Put: {
+                            Item: sItem,
+                            TableName: process.env.TABLE_STOCKS
+                        }
+                    }
+                ]
+            }
+
+            await dbClient.send(new TransactWriteItemsCommand(trCommand)).then(async(data) => {
+                const message = {
+                    productId: unmarshall(pItem).id,
+                    title: unmarshall(pItem).title,
+                    description: unmarshall(pItem).description,
+                    price: unmarshall(pItem).price,
+                    count: unmarshall(pItem).count
+
+                }
+                await sendMessageToSNS(message);
+            });
+        }
+    }
+    catch (error) {
+        console.error('create products request error', error);
+    }
+};
+
+const sendMessageToSNS = async(data) => {
+
+    try {
+        const snsClient = new SNSClient({ region: 'us-east-1' });
+        // Create publish parameters
+        var snsParams: PublishCommandInput = {
+            Message: JSON.stringify(data), /* required */
+            TopicArn: process.env.TOPIC_ARN
+        }
+        console.log('snsParams',snsParams);
+        await snsClient.send(new PublishCommand(snsParams))
+    }
+    catch (error) {
+        console.error('publishing request error', error);
+    }
+}
+
+export default catalogBatchProcess;
